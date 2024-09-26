2fb4e0efcb1c16cd1c7bf3b5c49e71866e826fe8
Yusuke Tanaka <wing0920@gmail.com>
2024-09-26 20:51:39 +0900

    add args

--- firefox.ts
+++ firefox.ts
@@ -3,6 +3,11 @@ import { firefox } from "npm:playwright@1.47.2";
 async function main() {
   await using browser = await firefox.launch({
     headless: true,
+    args: [
+      "--disable-gpu",
+      "--no-sandbox",
+      "--disable-dev-shm-usage",
+    ],
   });
   await using page = await browser.newPage();
   await page.goto("https://example.com");
