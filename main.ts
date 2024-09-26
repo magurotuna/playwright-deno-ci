8901839072c501791bbee811ed6736fedf61b7dc
Yusuke Tanaka <wing0920@gmail.com>
2024-09-26 20:23:15 +0900

    try

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 import { chromium } from "npm:playwright@1.47.2";
 
 async function main() {
-  await using browser = await chromium.launch({ headless: true });
+  await using browser = await chromium.launch({
+    headless: true,
+    args: ["--no-sandbox", "--headless=new", "--siable-dev-shm-usage"],
+  });
   await using page = await browser.newPage();
   await page.goto("https://example.com");
   const title = await page.title();
