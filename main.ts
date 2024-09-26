7bb68ac753ca766a9645c56344312aaef7914342
Yusuke Tanaka <wing0920@gmail.com>
2024-09-26 20:28:56 +0900

    try more options

--- main.ts
+++ main.ts
@@ -3,7 +3,15 @@ import { chromium } from "npm:playwright@1.47.2";
 async function main() {
   await using browser = await chromium.launch({
     headless: true,
-    args: ["--no-sandbox", "--headless=new", "--siable-dev-shm-usage"],
+    args: [
+      "--no-sandbox",
+      //"--headless=new",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--disable-gpu",
+      "--no-xshm",
+      "--disable-features=VizDisplayCompositor",
+    ],
   });
   await using page = await browser.newPage();
   await page.goto("https://example.com");
