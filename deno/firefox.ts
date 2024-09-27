dd730df4646082906b9fe981b2bf8ec42967c230
Yusuke Tanaka <wing0920@gmail.com>
2024-09-27 10:41:22 +0900

    what if we just open a browser

--- deno/firefox.ts
+++ deno/firefox.ts
@@ -1,7 +1,7 @@
 import { firefox } from "npm:playwright@1.47.2";
 
 async function main() {
-  await using browser = await firefox.launch({
+  await using _browser = await firefox.launch({
     headless: true,
     args: [
       "--no-sandbox",
@@ -11,10 +11,6 @@ async function main() {
       "media.hardware-video-decoding.enabled": false,
     },
   });
-  await using page = await browser.newPage();
-  await page.goto("https://example.com");
-  const title = await page.title();
-  console.log(`title: ${title}`);
 }
 
 if (import.meta.main) {
