244ebe059953fc9b8d663cfe4defe3bf60d5d3ae
Yusuke Tanaka <wing0920@gmail.com>
2024-09-27 10:48:07 +0900

    no options

--- deno/chromium.ts
+++ deno/chromium.ts
@@ -3,15 +3,6 @@ import { chromium } from "npm:playwright@1.47.2";
 async function main() {
   await using _browser = await chromium.launch({
     headless: true,
-    args: [
-      "--no-sandbox",
-      //"--headless=new",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--disable-gpu",
-      "--no-xshm",
-      "--disable-features=VizDisplayCompositor",
-    ],
   });
 }
 
--- deno/firefox.ts
+++ deno/firefox.ts
@@ -3,13 +3,6 @@ import { firefox } from "npm:playwright@1.47.2";
 async function main() {
   await using _browser = await firefox.launch({
     headless: true,
-    args: [
-      "--no-sandbox",
-    ],
-    firefoxUserPrefs: {
-      "gfx.webrender.all": false,
-      "media.hardware-video-decoding.enabled": false,
-    },
   });
 }
 
